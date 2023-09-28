16b139ce318f5f0455b5bca49374b09aff4ff8c5
Maison-A <maisonarroyo@protonmail.com>
2023-09-28 19:18:33 -0400

    missed adding files

---
+++ frontend/src/components/SignUpForm.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+form(@submit.prevent="createUser")
+    .form-group
+    label(for="email") Email
+    input#email.form-control(type="email" v-model="email" placeholder="Enter email")
+
+    .form-group
+    label(for="password") Password
+    input#password.form-control(type="password" v-model="password" placeholder="Enter password")
+
+    .form-group
+    label(for="username") Username
+    input#username.form-control(type="text" v-model="username" placeholder="Enter username")
+
+    button.btn.btn-primary(type="submit") Sign Up    
+</template>
+
+<script>
+import { useStore } from 'vuex'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      username: ''
+    }
+  },
+  methods: {
+    createUser() {
+      const payload = {
+        email: this.email,
+        password: this.password,
+        username: this.username
+      }
+      this.$store.dispatch('createUser', payload)
+    }
+  }
+}
+</script>
